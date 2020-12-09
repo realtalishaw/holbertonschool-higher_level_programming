94dc03cb2723f59a63342fb7032c6b968fdf372d
realtalishaw <misstalishawhite@gmail.com>
2020-12-09 02:20:43 +0000

    header file

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -21,4 +23,4 @@ void free_listint(listint_t *head);
 
 listint_t *insert_node(listint_t **head, int number);
 
-#endif 
+#endif
