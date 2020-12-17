900523bac08c3329bd8e450792ae9ed42c26d0a2
realtalishaw <misstalishawhite@gmail.com>
2020-12-17 01:04:04 +0000

    replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+        if idx >= len(my_list):
+            return my_list
+        newList = []
+        for i in range(len(my_list)):
+            newList.append(my_list[i])
+        newList[idx] = element
+        return newList
