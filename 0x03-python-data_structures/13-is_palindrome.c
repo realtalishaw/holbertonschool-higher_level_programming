bead6c456e01b807ca7dfc2a0323e560db50d55f
realtalishaw <misstalishawhite@gmail.com>
2021-01-03 14:32:10 +0000

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - fjjfs
+ * @head: dfsd
+ *
+ * Returns: fdsd
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp;
+	char *buffer;
+	int i = 0;
+	int end = 0;
+
+	buffer = malloc(sizeof(head));
+
+	/*if (!buffer)
+	  return (NULL);*/
+
+	if (!*head || !head)
+		return (1);
+
+	tmp = *head;
+	if (!tmp->next)
+		return (1);
+
+	while (tmp)
+	{
+		buffer[end] = tmp->n;
+		tmp = tmp->next;
+		end++;
+	}
+
+	end = end - 1;
+	for (; end >= 0 && i <= end; end--, i++)
+	{
+		if (buffer[end] != buffer[i])
+			return (0);
+	}
+	return (1);
+}
