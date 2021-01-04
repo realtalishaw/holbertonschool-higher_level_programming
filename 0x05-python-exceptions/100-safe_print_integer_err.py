ad2a0f283491f4a6717b41846c6adf63008d4ed5
realtalishaw <misstalishawhite@gmail.com>
2021-01-04 13:12:33 +0000

    safe print

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err:
+        stderr.write("Exception: {}\n".format(err))
+        return False
+    return True
