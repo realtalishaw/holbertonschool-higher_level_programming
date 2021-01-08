be5bc238ac148a7ad73556419a50e9fd74404c71
realtalishaw <misstalishawhite@gmail.com>
2021-01-08 16:19:26 +0000

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"ghksflkshdlf hh lshdf jsdkjfklsdj kljfksdjfkjd"
+
+
+class Square:
+    """g lks jdks jl mgddffsdf sdfsd fsd gsfs sfgs s """
+    def __init__(self, size):
+        self.__size = size
