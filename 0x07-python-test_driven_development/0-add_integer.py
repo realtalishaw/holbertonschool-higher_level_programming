3b9de7da0d1a1175fc5c5392c587f641b7fa481f
realtalishaw <misstalishawhite@gmail.com>
2021-01-09 01:30:31 +0000

    removed in file tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-from doctest import testmod
+
 
 def add_integer(a, b=98):
-    """
-    define input and expected output:
-    >>> add_integer(1, 2)
-    3
-    >>> add_integer(100, -2)
-    98
-    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
@@ -18,6 +11,3 @@ def add_integer(a, b=98):
     if type(b) is float:
         b = int(b)
     return a + b
-
-if __name__ == "__main__":
-    testmod(name="add_integer", verbose = True)
