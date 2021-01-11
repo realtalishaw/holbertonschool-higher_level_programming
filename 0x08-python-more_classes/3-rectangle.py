921d6f2d0e9ce4d7ec8302edf1f49371b200bc0c
realtalishaw <misstalishawhite@gmail.com>
2021-01-11 14:34:48 +0000

    string representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"ghksflkshdlf hh lshdf jsdkjfklsdj kljfksdjfkjd"
+
+
+class Rectangle:
+    """g lks jdks jl mgddffsdf sdfsd fsd gsfs sfgs s """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return self.width * 2 + self.height * 2
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        rectangle = "#" * self.width
+        return "\n".join(list(rectangle for i in range(self.height)))
