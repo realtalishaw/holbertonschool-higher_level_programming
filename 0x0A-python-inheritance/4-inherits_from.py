624290800783971bb3c7079ed509bf4005d95b6a
realtalishaw <misstalishawhite@gmail.com>
2021-01-20 00:24:39 +0000

    doc

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+"kg gjjfd gdf gpdfkd; g"
+
+
 def inherits_from(obj, a_class):
+    """kgdfkjg odfjko pogfodp dfgv"""
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
