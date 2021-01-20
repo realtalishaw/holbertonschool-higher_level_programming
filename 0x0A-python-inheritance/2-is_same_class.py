064992fa9db976b053056e76b42884af3144c8cd
realtalishaw <misstalishawhite@gmail.com>
2021-01-20 00:22:36 +0000

    doc

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+" fdlgjdf dfkdg gfdgdfgdfg dgf"
+
+
 def is_same_class(obj, a_class):
+    """gfk kfgjdflkj gjdflkj gldfjgld"""
     if type(obj) is a_class:
         return True
     else:
