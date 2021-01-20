b2755eb55b29249837ae44595d6ab8ebd022933a
realtalishaw <misstalishawhite@gmail.com>
2021-01-20 00:15:12 +0000

    documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+
 "kgjfkljg lgflkjgd gjflg d dgjfdgd"
 
+
 def lookup(obj):
-    ''' jfkjgfd gfkl mgflk mglkfd klgjfkl """
+    """jfkjgfd gfkl mgflk mglkfd klgjfkl """
     return dir(obj)
