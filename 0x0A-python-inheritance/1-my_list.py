d71048df5b10478a9e63e1faf742c0772021896b
realtalishaw <misstalishawhite@gmail.com>
2021-01-20 00:16:20 +0000

    documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 
+"fldgjldfjgkjdfkl dfljgl jfdlgjdl; gl;fdl"
+
+
 class MyList(list):
+    """fkg jdgjdkf jlkdgf klsdlkj kdfj"""
     def print_sorted(self):
         print(sorted(self))
