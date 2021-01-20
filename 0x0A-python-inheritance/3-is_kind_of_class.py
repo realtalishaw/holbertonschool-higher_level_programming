49f65ee4186bdedcb6e777a4ee2ac18767a960d4
realtalishaw <misstalishawhite@gmail.com>
2021-01-20 00:23:38 +0000

    doc

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+
+"kjdflkjdl gjf jglkdfgdfgdf"
+
+
 def is_kind_of_class(obj, a_class):
+    """jj jglfkjkd jlkgjf jgfdj g"""
     if isinstance(obj, a_class):
         return True
     else:
