42f175bad32e08c3d6a4d418f68ac0a3a0f0ec69
realtalishaw <misstalishawhite@gmail.com>
2021-01-31 19:15:12 +0000

    ok

--- 0x0B-python-input_output/1-number_of_lines.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def number_of_lines(filename=""):
-    """kdjfdf dfsdfss"""
-    lines = 0
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            lines += 1
-    return lines
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def write_file(filename="", text=""):
+    """ejhkf dsfdsgs dfdss"""
+    with open(filename, 'w') as f:
+        return f.write(text)
