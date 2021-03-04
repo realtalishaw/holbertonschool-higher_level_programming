410f386634d035edea26445da8772e6dd8bcf199
realtalishaw <misstalishawhite@gmail.com>
2021-03-04 03:34:25 +0000

    6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- ndfksn sdf dfs fdsfs dfsdfs
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+       id int NOT NULL AUTO_INCREMENT,
+       name varchar(256) NOT NULL,
+       PRIMARY KEY (id));
