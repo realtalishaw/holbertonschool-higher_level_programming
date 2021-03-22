3fdc2fc28c850016b6dfecc8fde8835535ec276a
realtalishaw <misstalishawhite@gmail.com>
2021-03-22 23:18:07 +0000

    stupid

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const stupid = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const stupid = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < stupid.length; i++) {
   console.log(stupid[i]);
 }
