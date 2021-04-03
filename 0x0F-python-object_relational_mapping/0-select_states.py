daeab1414eae1dcad4b2bcd43db5f49b95996c5d
realtalishaw <misstalishawhite@gmail.com>
2021-04-03 00:36:35 +0000

    fix typo

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 ''' this is a comment '''
 if __name__ == "__main__":
-    import MYSQLdb
+    import MySQLdb
     import sys
 
-conn = MYSQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                        passwd=sys.argv[2], db=sys.argv[3])
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
